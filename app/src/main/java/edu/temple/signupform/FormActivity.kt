07ce5ed0a438baa09f8c0127f55ebcbd5c5ebc33
KingRDX <muledi9@gmail.com>
2022-02-06 21:14:21 -0800

    The app can check to see if the fields to see if there is text.

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -2,10 +2,28 @@ package edu.temple.signupform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val saveButton = findViewById<Button>(R.id.Save)
+        val textPerson = findViewById<TextView>(R.id.TextPersonName)
+        val textEmail = findViewById<TextView>(R.id.TextEmailAddress)
+        val textPassword = findViewById<TextView>(R.id.TextPassword)
+        val textPasswordC = findViewById<TextView>(R.id.TextPasswordConfirmation)
+       saveButton.setOnClickListener{
+            if(textPassword.length() == 0 ||  textPasswordC.length() == 0
+                ||  textEmail.length() == 0 ||  textPerson.length() == 0){
+                Toast.makeText(applicationContext,"Fill out all Fields",Toast.LENGTH_LONG).show()
+            }
+        }
+
     }
 }
